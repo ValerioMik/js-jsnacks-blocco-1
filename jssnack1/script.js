bdced3cbe7a9e204daeb96a72869885f810493b5
ValerioMik <valerio.avallone@hotmail.com>
2021-10-28 15:13:02 +0200

    primo esercizio terminato

--- jssnack1/script.js
+++ jssnack1/script.js
@@ -1,10 +1,26 @@
 /* 1 - L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore. */
 
-//1.faccio si che l'utente inserica il primo numero
-//2.faccio si che l'utente inserica il secondo numero
+//DONE=> 1.faccio si che l'utente inserica il primo numero
+//DONE=> 2.faccio si che l'utente inserica il secondo numero
 //3. confronto i numerio inseriti 
     //3.1 se il primo numero e maggiore lom stampo
     //3.2 altrimenti stampo il secondo
          
     
+const priMOnum = parseInt(prompt("Inserisci il primo numero"));
+const secCOnum = parseInt(prompt("Inserisci il secondo numero"));
+
+
+let risultato;
+if(priMOnum > secCOnum){
+    console.log(priMOnum);
+
+}else if(priMOnum < secCOnum){
+    console.log(secCOnum);
+
+}else{
+    console.log("hai sbagliato");
+}; 
+   
+   
