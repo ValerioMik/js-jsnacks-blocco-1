962fd6c5ba57391c74f5d783dfdd926d8b103f56
ValerioMik <valerio.avallone@hotmail.com>
2021-10-28 14:35:06 +0200

    primo esercizio commentata la logica

---
+++ jssnack1/script.js
@@ -0,0 +1,10 @@
+/* 1 - L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+//1.faccio si che l'utente inserica il primo numero
+//2.faccio si che l'utente inserica il secondo numero
+//3. confronto i numerio inseriti 
+    //3.1 se il primo numero e maggiore lom stampo
+    //3.2 altrimenti stampo il secondo
+         
+    
